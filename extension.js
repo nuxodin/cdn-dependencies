f2807b44f4b9189f9d52ada507bfdf60bf0d51ff
Tobias Buschor <tobias.buschor@shwups.ch>
2023-03-24 10:02:48 +0000

    Mar 24, 2023, 11:02 AM

--- extension.js
+++ extension.js
@@ -242,3 +242,19 @@ async function xGet(url, data) {
     req.end();
   });
 }
+
+function xGet(url, ok, fail) {
+	http.get(url, function(res){
+		var body = '';
+		res.on('data', function(chunk){
+			body += chunk;
+		});
+		res.on('end', function(){
+			var fbResponse = JSON.parse(body);
+			console.log("Got a response: ", fbResponse.picture);
+		});
+	}).on('error', function(e){
+		  console.log("Got an error: ", e);
+	});
+
+}
