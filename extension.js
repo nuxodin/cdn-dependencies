3bd4657ced420ad0461a64bc67ea7339225f453b
Tobias Buschor <tobias.buschor@shwups.ch>
2023-03-24 10:08:06 +0000

    Mar 24, 2023, 11:08 AM

--- extension.js
+++ extension.js
@@ -207,42 +207,6 @@ function regEscape(str) {
 
 const https = require('https')
 
-function xxGet(url, data) {
-  const dataString = JSON.stringify(data)
-  const options = {
-    //method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      //'Content-Length': dataString.length,
-    },
-    timeout: 1000, // in ms
-  }
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(url, options, (res) => {
-      if (res.statusCode < 200 || res.statusCode > 299) {
-        return reject(new Error(`HTTP status code ${res.statusCode}`))
-      }
-      const body = [];
-      res.on('data', (chunk) => body.push(chunk))
-      res.on('end', () => {
-        const resString = Buffer.concat(body).toString();
-		const obj = JSON.parse(resString);
-        resolve(obj)
-      })
-    })
-    req.on('error', (err) => {
-      reject(err)
-    })
-    req.on('timeout', () => {
-      req.destroy()
-      reject(new Error('Request time out'))
-    })
-    //req.write(dataString);
-    req.end();
-  });
-}
-
 function xGet(url) {
 	return new Promise((resolve, reject) => {
 		https.get(url, function(res){
